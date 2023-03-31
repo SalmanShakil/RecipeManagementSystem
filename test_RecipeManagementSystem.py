d19f0177e46253c30b7379a845ffe2f801af9f4a
Salman <60098379@cna-qatar.edu.qa>
2023-04-01 00:40:07 +0300

    Unit test for view and delete recipe

--- test_RecipeManagementSystem.py
+++ test_RecipeManagementSystem.py
@@ -8,3 +8,25 @@ class TestRMS(unittest.TestCase):
         management.addRecipe(recipe)
         addedRecipe = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 'appetizer', 'rating': 5}
         self.assertIn(addedRecipe, management.recipes)
+    
+
+    def test_viewRecipe(self):
+        management = RecipeManagementSystem.RMS()
+        recipe1 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", 5)
+        recipe2 = RecipeManagementSystem.Recipe("pizza", "chicken, ranch sauce, mushrooms", "bake for 30 minutes", "main", 5)         
+        management.addRecipe(recipe1)
+        management.addRecipe(recipe2)
+        management.viewRecipe(2)
+        self.assertEqual([management.recipes[0]['id'],management.recipes[0]['name']],[1,'soup'])
+        self.assertEqual([management.recipes[1]['id'],management.recipes[1]['name']],[2,'pizza'])
+    
+
+    def test_deleteRecipe(self):
+        management = RecipeManagementSystem.RMS()
+        recipe1 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", 5)
+        recipe2 = RecipeManagementSystem.Recipe("pizza", "chicken, ranch sauce, mushrooms", "bake for 30 minutes", "main", 5)         
+        management.addRecipe(recipe1)
+        management.addRecipe(recipe2)
+        management.deleteRecipe(1)
+        addedRecipe = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 'appetizer', 'rating': 5}
+        self.assertNotIn(addedRecipe, management.recipes)
