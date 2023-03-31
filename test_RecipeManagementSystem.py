5f4d77695bdf2ba619672403ba89b13f2b999ffd
Salman <60098379@cna-qatar.edu.qa>
2023-04-01 00:45:33 +0300

    Added multiple fail unit test for diff add recipe inputs

--- test_RecipeManagementSystem.py
+++ test_RecipeManagementSystem.py
@@ -62,3 +62,55 @@ class TestRMS(unittest.TestCase):
         addedRecipe1 = {'id': 2, 'name': 'pizza', 'ingredients': 'chicken, ranch sauce, mushrooms', 'instructions': 'bake for 30 minutes', 'category': 'main', 'rating': 5}
         self.assertIn(addedRecipe, management.recipes)
         self.assertIn(addedRecipe1, management.recipes)
+    
+
+    def test_addRecipe_1(self):      
+        management = RecipeManagementSystem.RMS()    
+        recipe = RecipeManagementSystem.Recipe("", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", 5)
+        recipe1 = RecipeManagementSystem.Recipe(-1, "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", 5)
+        management.addRecipe(recipe)
+        management.addRecipe(recipe1)
+        addedRecipe = {'id': 1, 'name': '', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 'appetizer', 'rating': 5}
+        addedRecipe1 = {'id': 1, 'name': -1, 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 'appetizer', 'rating': 5}
+        self.assertNotIn(addedRecipe, management.recipes)
+        self.assertNotIn(addedRecipe1, management.recipes)
+    
+
+    def test_addRecipe_2(self):
+        management = RecipeManagementSystem.RMS()    
+        recipe1 = RecipeManagementSystem.Recipe("soup", "", "boil all ingredients together and leave for 30 minutes", "appetizer", 5)
+        management.addRecipe(recipe1)
+        addedRecipe1 = {'id': 1, 'name': 'soup', 'ingredients': '', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 'appetizer', 'rating': 5}
+        self.assertNotIn(addedRecipe1, management.recipes)
+    
+
+    def test_addRecipe_3(self):
+        management = RecipeManagementSystem.RMS()    
+        recipe1 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "", "appetizer", 5)
+        management.addRecipe(recipe1)
+        addedRecipe1 = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': '', 'category': 'appetizer', 'rating': 5}
+        self.assertNotIn(addedRecipe1, management.recipes)
+    
+
+    def test_addRecipe_4(self):
+        management = RecipeManagementSystem.RMS()    
+        recipe1 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", 23, 5)
+        recipe2 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "", 5)
+        management.addRecipe(recipe1)
+        management.addRecipe(recipe2)
+        addedRecipe1 = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': 23, 'rating': 5}
+        addedRecipe2 = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': 'boil all ingredients together and leave for 30 minutes', 'category': '', 'rating': 5}
+        self.assertNotIn(addedRecipe1, management.recipes)
+        self.assertNotIn(addedRecipe2, management.recipes)
+    
+
+    def test_addRecipe_5(self):
+        management = RecipeManagementSystem.RMS()    
+        recipe1 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", "good")
+        recipe2 = RecipeManagementSystem.Recipe("soup", "tomato, tomato paste, water, spices", "boil all ingredients together and leave for 30 minutes", "appetizer", "")
+        management.addRecipe(recipe1)
+        management.addRecipe(recipe2)
+        addedRecipe1 = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': '', 'category': 'appetizer', 'rating': 'good'}
+        addedRecipe2 = {'id': 1, 'name': 'soup', 'ingredients': 'tomato, tomato paste, water, spices', 'instructions': '', 'category': 'appetizer', 'rating': ''}
+        self.assertNotIn(addedRecipe1, management.recipes)
+        self.assertNotIn(addedRecipe2, management.recipes)
