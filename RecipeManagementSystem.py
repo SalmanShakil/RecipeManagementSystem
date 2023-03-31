7caacadd5bc1e11a2528e812ca48926649cdc050
Salman <60098379@cna-qatar.edu.qa>
2023-03-31 19:54:55 +0300

    Modified Skeleton

---
+++ RecipeManagementSystem.py
@@ -0,0 +1,44 @@
+import sys,json
+class Recipe:
+    def __init__(self,name,ingredients,instructions,category,rating):
+        self.name = name
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.category = category
+        self.rating = rating
+
+class RMS:
+    def __init__(self):
+        self.recipes = []
+        
+        
+    def addRecipe(self):
+        return
+
+
+    def viewRecipe(self):
+        return
+            
+
+    def deleteRecipe(self):
+        return
+
+
+    def editRecipe(self):
+        return
+
+
+    def exportRecipe(self):
+        return
+
+
+    def importRecipe(self):
+        return
+
+
+    def askUserOptions(self):
+        return
+           
+            
+    def exitRecipe(self):
+        return
