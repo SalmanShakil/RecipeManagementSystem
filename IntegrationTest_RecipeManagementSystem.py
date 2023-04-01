e5d6acd37e81a2f5031a8173e63b42ea6ab5b9b6
Salman <60098379@cna-qatar.edu.qa>
2023-04-01 17:30:45 +0300

    Integration test for add, view and export recipe

--- IntegrationTest_RecipeManagementSystem.py
+++ IntegrationTest_RecipeManagementSystem.py
@@ -71,6 +71,19 @@ class TestRMSIntegration(unittest.TestCase):
         management.deleteRecipe(1)
         # edit a recipe that does not exist
         management.editRecipe(10, 1, "Spaghetti Carbonara")
+    
+
+    def test_add_view_export_recipe(self):
+        management = RecipeManagementSystem.RMS()
+        recipe1 = RecipeManagementSystem.Recipe("Chicken Curry", "chicken, curry paste, coconut milk, vegetables", "1. Heat oil in a pot. 2. Add curry paste and fry for 1 minute. 3. Add chicken and fry until browned. 4. Add coconut milk and simmer for 20 minutes. 5. Add vegetables and simmer for another 10 minutes.", "Curry", 4)
+        management.addRecipe(recipe1)
+        captured_output = io.StringIO()          
+        sys.stdout = captured_output             
+        management.viewRecipe(1)                   
+        sys.stdout = sys.__stdout__              
+        expected_output = "\nRecipe Name: Chicken Curry\nIngredients Required: chicken, curry paste, coconut milk, vegetables\nInstructions: 1. Heat oil in a pot. 2. Add curry paste and fry for 1 minute. 3. Add chicken and fry until browned. 4. Add coconut milk and simmer for 20 minutes. 5. Add vegetables and simmer for another 10 minutes.\nCategory: Curry\nRating: 4\n"
+        self.assertEqual(captured_output.getvalue(), expected_output)
+        management.exportRecipe("Curries")
 
 if __name__ == '__main__':
     unittest.main()
